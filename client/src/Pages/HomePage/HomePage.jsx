cdbb4dfff23f2c044f6af3fbc06d2584f4d1f328
rotem-bendavid <bendarotem@gmail.com>
2022-07-28 12:25:53 +0300

    add homePage and change background img

---
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./homePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="home-page-container">
+      <div className="home-center">
+            <h1>Welcome To Slinky!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
