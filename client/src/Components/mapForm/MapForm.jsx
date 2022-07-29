ed7a37501d1cd5f3cb07d848a19f76848c588c9d
rotem-bendavid <bendarotem@gmail.com>
2022-07-29 14:44:30 +0300

    mapForm fix headline style

--- client/src/Components/mapForm/MapForm.jsx
+++ client/src/Components/mapForm/MapForm.jsx
@@ -25,7 +25,7 @@ const MapForm = () => {
     }/>
     {/* : */}
     <div className="map-center">
-      <h1>map</h1>
+      <h1>Choose your office:</h1>
       <div id="mapBackGround"> 
       {officePosition.map((element) =>       
          <button className="btn" id={element} key={element} onClick={()=> getBooking(officeId,element)}>{element}</button>
