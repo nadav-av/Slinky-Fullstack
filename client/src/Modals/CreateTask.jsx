86f8ee5bc49fbd2cc67f61ebc19f2d878dfaf68f
Shalev Sharf <81631445+shalevsh@users.noreply.github.com>
2022-08-01 16:49:14 +0300

    bla bla

--- client/src/Modals/CreateTask.jsx
+++ client/src/Modals/CreateTask.jsx
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormControl,InputLabel,Input,FormHelperText,Button } from '@mui/material';
 import "./createTask.css";
-
+import notificationClient from "../Services/notificationClient";
 
 const CreateTask =({modal,toggle}) =>{
+  const [content, setContent] = useState("");
+  const [officeId, setOfficeId] = useState(1);
+  const [category, setCategory] = useState("idea");
+
+  const addNotification = async (e) => {
+    e.preventDefault();
+       try {
+          await notificationClient.addNotification(
+            officeId,
+            content,
+            category,
+          )
+          alert('Notification success!');
+    } catch {
+      console.err("err");
+      alert("Notification Failed.");
+    }
+    
+  }
     return(
         <>
         <div className="createTaskWrapper">
         <div className="create-task-container"> 
+        <h1>Notification</h1>
         <FormControl>
-        <InputLabel htmlFor="my-input">Create task</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormHelperText id="my-helper-text">.......</FormHelperText>
-        <Button variant="raised">Submit</Button>
+        <InputLabel htmlFor="office-input">Office number</InputLabel>
+        <Input id="office-input"  onChange={(e) => setOfficeId(e.target.value)} />
+        </FormControl>
+        <br></br>
+        <br></br>
+        <FormControl>
+        <InputLabel htmlFor="category-input">Category</InputLabel>
+        <Input id="category-input"  onChange={(e) => setCategory(e.target.value)} />
+        </FormControl>
+        <br></br>
+        <br></br>
+        <FormControl>
+        <InputLabel htmlFor="content-input">Content</InputLabel>
+        <Input id= "content-input"  onChange={(e) => setContent(e.target.value)} />
+        <br></br>
+        <Button onclick={addNotification()} variant="raised">Submit</Button>
       </FormControl>
       </div>
       </div>
--- client/src/Pages/NotificationPage/Notification.jsx
+++ client/src/Pages/NotificationPage/Notification.jsx
@@ -3,6 +3,9 @@ import "./notification.css";
 import NotificationForm from "../../Components/NotificationForm/NotificationForm";
 import CreateTask from "../../Modals/CreateTask";
 import GenericModal from "../../Components/GenericModal/genericModal"
+import notificationClient from "../../Services/notificationClient";
+import { INVALID_NOTIFICATIONS } from "../../Services/Consts";
+
 let serverMockData =
 [{idOffice:1,userName:"SHALEV",content:"toilet full of a shit",category:"review"},
 {idOffice:1,userName:"NADAV",content:"if you wand cuddle smile pls",category:"review"},
@@ -16,25 +19,38 @@ let serverMockData =
 const Notification = () => {
 const [data, setData] = useState([]);
 const [modal, setModal] = useState(false);
-useEffect(() => {},[serverMockData]);
+
+useEffect(() => {
+  getNotifications();
+},[data]);
+
+
+const getNotifications = async ()=>{
+  try {
+        console.log(await notificationClient.getNotifications(),"rotem")
+        setData(await notificationClient.getNotifications());
+      } catch (err) {
+      console.error("err");
+    }
+}
+
 
 const toggle = () => setModal(!modal);
-const addTask = () =>{
-  setData(...data,true)
-}
+
 
   return (
     <>
      {modal === true ? 
-    <GenericModal open ={modal} onClose ={()=> {setModal(false)}} content={<CreateTask toggle ={toggle} modal ={modal}/>}onSubmit={()=>{}}/>
+    <GenericModal open ={modal} onClose ={()=> {setModal(false)}} content={<CreateTask toggle ={toggle} modal ={modal}/>}/>
     :
     <div className="notification-container">
       <button className="btn-primary" onClick={() => setModal(true)}> Create Task </button>
+
       <div className="notification-container-array">
-      {serverMockData.map((element) =>       
+      {data.map((element) =>       
          <NotificationForm data={element} key={element.userName}></NotificationForm>
       )}
-    </div>
+      </div>
     </div>
       }
     </>
