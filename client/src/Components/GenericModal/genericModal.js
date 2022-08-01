0efc4ae5d7a9b6baf5173f1f35d6bfa8b6bc8587
vico958 <74486817+vico958@users.noreply.github.com>
2022-08-01 13:10:03 +0300

    fix Modal

--- client/src/Components/GenericModal/genericModal.js
+++ client/src/Components/GenericModal/genericModal.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import {Button} from '@mui/material';
 import Modal from '@mui/material/Modal';
 import './genericModal.css';
-import {DialogContentContainer } from "monday-ui-react-core";
 
 const GenericModal = ({ open, onClose, content }) => {
-
     return (
         <div>
-        <DialogContentContainer type={DialogContentContainer.types.MODAL}>
-            <div>
+        <Modal open={open} onClose={onClose}>
+            <div className='content-container'>
                 {content}
             </div>
-        </DialogContentContainer >
+        </Modal>
         </div>
     )
 }
