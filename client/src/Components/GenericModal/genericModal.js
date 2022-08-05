d82d10a29950446b77d39e0eed29ee6ef556b293
nadav-av <avr.nadav@gmail.com>
2022-08-05 12:46:04 +0300

    rotem

--- client/src/Components/GenericModal/genericModal.js
+++ client/src/Components/GenericModal/genericModal.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import Modal from '@mui/material/Modal';
-import './genericModal.css';
+import React from "react";
+import Modal from "@mui/material/Modal";
+import "./genericModal.css";
 
 const GenericModal = ({ open, onClose, content }) => {
-    return (
-        <div>
-        <Modal open={open} onClose={onClose}>
-            <div className='content-container'>
-                {content}
-            </div>
-        </Modal>
-        </div>
-    )
-}
+  return (
+    <div className="modal">
+      <Modal open={open} onClose={onClose}>
+        <div className="content-container">{content}</div>
+      </Modal>
+    </div>
+  );
+};
 
-export default GenericModal
+export default GenericModal;
